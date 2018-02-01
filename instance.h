d437aa41dba07990a60dfa48a701a946524f0dcd
Andrew Li <andrewli@scspcxxx>
2018-02-01 16:09:21 -0500

    wip-full-parent-set

--- instance.h
+++ instance.h
@@ -2,6 +2,7 @@
 #define INSTANCE_H 
 
 #include <vector>
+#include <list>
 #include <unordered_set>
 #include "variable.h"
 #include "types.h"
@@ -15,13 +16,19 @@ class Instance {
     int getM() const;
     bool isConstraint(int a, int b) const;
 
+    void sortAllParents();
+    std::list< std::pair<int, int> > &getParentList();
+    int numParents() const;
+
     Variable &getVar(int i);
     const Ancestral &getAncestral(int i) const;
+
 
     friend std::ostream& operator<<(std::ostream &os, const Instance& I);
 
   private:
     int n, m;
+    std::list< std::pair<int,int> > allParentSets;
     std::vector<Variable> vars;
     std::vector<Ancestral> ancestralConstraints;
     std::unordered_set<int> orderConstraints;
