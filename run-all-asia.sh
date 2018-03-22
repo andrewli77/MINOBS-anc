9dd3d5846736a7ddf17a849d1dc2c8adb7259e41
Andrew Li <andrewli@scspcxxx>
2018-03-22 15:12:53 -0400

    WIP

--- run-all-asia.sh
+++ run-all-asia.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-eval "./run-all.sh data/asia_1000_1.BIC data/constraints/asia 10"
-eval "./run-all.sh data/asia_1000_2.BIC data/constraints/asia 10"
-eval "./run-all.sh data/asia_1000_3.BIC data/constraints/asia 10"
-eval "./run-all.sh data/asia_1000_4.BIC data/constraints/asia 10"
-eval "./run-all.sh data/asia_1000_5.BIC data/constraints/asia 10"
+eval "./run-all.sh data/asia_250_1.BIC data/constraints/asia 10"
+eval "./run-all.sh data/asia_250_2.BIC data/constraints/asia 10"
+eval "./run-all.sh data/asia_250_3.BIC data/constraints/asia 10"
+eval "./run-all.sh data/asia_250_4.BIC data/constraints/asia 10"
+eval "./run-all.sh data/asia_250_5.BIC data/constraints/asia 10"
 
