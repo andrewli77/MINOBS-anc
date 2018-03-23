792ecf561320adcac82090843eb2084dc82c9b21
Andrew Li <andrewli@scspcxxx>
2018-03-23 19:26:19 -0400

    WIP

--- run-all-alarm.sh
+++ run-all-alarm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-eval "./run-all.sh data/alarm_1000_1.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_1000_2.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_1000_3.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_1000_4.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_1000_5.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_1000_1.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_1000_2.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_1000_3.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_1000_4.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_1000_5.BIC data/constraints/alarm 10"
 
-eval "./run-all.sh data/alarm_4000_1.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_4000_2.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_4000_3.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_4000_4.BIC data/constraints/alarm 10"
-eval "./run-all.sh data/alarm_4000_5.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_4000_1.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_4000_2.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_4000_3.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_4000_4.BIC data/constraints/alarm 10"
+eval "sbatch --time=12:00:00 ./run-all.sh data/alarm_4000_5.BIC data/constraints/alarm 10"
--- run-all.sh
+++ run-all.sh
@@ -5,7 +5,6 @@ dir=$2
 timelimit=$3
 
 for file in $dir/*; do
-	cmd="sbatch --time=12:00:00 ./run-one-case.sh $scores $file"
+	cmd="./run-one-case.sh $scores $file"
 	eval "$cmd"
-	sleep 1
 done
