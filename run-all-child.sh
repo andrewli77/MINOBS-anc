9359a36956f28c4bfaabc747bf4ac277aed20b0b
Andrew Li <andrewli@scspcxxx>
2018-03-28 19:57:39 -0400

    WIP

--- run-all-child.sh
+++ run-all-child.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_500_1.BIC data/constraints/child data/child_500_1_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_500_2.BIC data/constraints/child data/child_500_2_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_500_3.BIC data/constraints/child data/child_500_3_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_500_4.BIC data/constraints/child data/child_500_4_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_500_5.BIC data/constraints/child data/child_500_5_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_500_6.BIC data/constraints/child data/child_500_6_results"
-
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_2000_1.BIC data/constraints/child data/child_2000_1_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_2000_2.BIC data/constraints/child data/child_2000_2_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_2000_3.BIC data/constraints/child data/child_2000_3_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_2000_4.BIC data/constraints/child data/child_2000_4_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_2000_5.BIC data/constraints/child data/child_2000_5_results"
-eval "sbatch --time=100:00:00 ./run-all.sh data/child_2000_6.BIC data/constraints/child data/child_2000_6_results"
+eval "./run-all.sh data/child_2000_1.BDeu data/constraints/child data/child_2000_1_results"
+eval "./run-all.sh data/child_2000_2.BDeu data/constraints/child data/child_2000_2_results"
+eval "./run-all.sh data/child_2000_3.BDeu data/constraints/child data/child_2000_3_results"
+eval "./run-all.sh data/child_2000_4.BDeu data/constraints/child data/child_2000_4_results"
+eval "./run-all.sh data/child_2000_5.BDeu data/constraints/child data/child_2000_5_results"
+eval "./run-all.sh data/child_2000_6.BDeu data/constraints/child data/child_2000_6_results"
