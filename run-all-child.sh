4924c93c6fb726acceb954b27208d564bc926a23
Andrew Li <andrewli@scspcxxx>
2018-03-28 21:26:37 -0400

    WIP

--- run-all-child.sh
+++ run-all-child.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-eval "./run-all.sh data/child_2000_1.BDeu data/constraints/child data/child_2000_1_results"
-eval "./run-all.sh data/child_2000_2.BDeu data/constraints/child data/child_2000_2_results"
-eval "./run-all.sh data/child_2000_3.BDeu data/constraints/child data/child_2000_3_results"
-eval "./run-all.sh data/child_2000_4.BDeu data/constraints/child data/child_2000_4_results"
-eval "./run-all.sh data/child_2000_5.BDeu data/constraints/child data/child_2000_5_results"
-eval "./run-all.sh data/child_2000_6.BDeu data/constraints/child data/child_2000_6_results"
+eval "./run-one-case.sh data/child_2000_1.BIC data/constraints/child/child.0-1 data/child_2000_1_results"
+eval "./run-one-case.sh data/child_2000_2.BIC data/constraints/child/child.0-1 data/child_2000_2_results"
+eval "./run-one-case.sh data/child_2000_3.BIC data/constraints/child/child.0-1 data/child_2000_3_results"
+eval "./run-one-case.sh data/child_2000_4.BIC data/constraints/child/child.0-1 data/child_2000_4_results"
+eval "./run-one-case.sh data/child_2000_5.BIC data/constraints/child/child.0-1 data/child_2000_5_results"
+eval "./run-one-case.sh data/child_2000_6.BIC data/constraints/child/child.0-1 data/child_2000_6_results"
